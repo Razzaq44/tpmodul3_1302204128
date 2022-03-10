89c4c5d71cf65abad82e3fa48f84d814ebb024e5
razzaq <razzaq@student.telkomuniversity.ac.id>
2022-03-10 22:32:24 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul3_1302204128/Program.cs
+++ tpmodul3_1302204128/Program.cs
@@ -1,9 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-class KodePos
+﻿namespace tpmodul3_1302204128
 {
-    public int getKodePos
+    public class KodePos
     {
+        static void Main()
+        {
+            Console.Write("Masukan Nama Kelurahan: ");
+            string namaKelurahan = Console.ReadLine();
+            Console.WriteLine(getKodePos(namaKelurahan));
+        }
+        static int getKodePos(string namaKelurahan)
+        {
+            string[] kelurahan = { "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura", "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja" };
+            int[] kodePosKelurahan = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+            int maxKelurahan = kelurahan.Length - 1;
 
+            int listKelurahan = 40273;
+            int kelurahanLevel = 0;
+            while ((listKelurahan == 40273) && (kelurahanLevel < maxKelurahan))
+            {
+                if (namaKelurahan == kelurahan[kelurahanLevel])
+                {
+                    listKelurahan = kodePosKelurahan[kelurahanLevel];
+                }
+                kelurahanLevel = kelurahanLevel + 1;
+            }
+            return listKelurahan;
+        }
     }
 }
